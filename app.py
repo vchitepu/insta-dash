1323c5b2ef63255dda9396996ea9a76381c51f57
vchitepu <vinaychitepu@gmail.com>
2023-09-03 17:13:26 -0400

    Added redfin scraper

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
 import streamlit as st
-import numpy as np
+import os
+import shutil
+from streamlit_imagegrid import streamlit_imagegrid
+from lib.redfin import RedFinImages
 
-st.title('Test Title')
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
+redfin = RedFinImages()
+
+
+st.set_page_config(
+	page_title="Redfin Scraper",
+	layout="wide"
+)
+
+st.title("RedFin Image Scraper")
+
+with st.form(key="redfin_user_form"):
+	url = st.text_input(label="Enter RedFin URL or Home Address ")
+	submit = st.form_submit_button(label="Submit")
+
+if submit:
+	dir = redfin.download_images(url)
+	fp = open('images/{dir}.zip'.format(dir=dir), "rb")
+	st.download_button(
+		label="Download Images",
+		data=fp,
+		file_name='images.zip',
+		mime='application/zip'
+	)
+	fp.close()
+	shutil.rmtree('images')
